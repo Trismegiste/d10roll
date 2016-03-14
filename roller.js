11063bc0fa799f6c7d7bb1d4d51c5ac32f4e629e
Trismegiste <flo@trismegiste.net>
2016-03-14 20:19:35 +0100

    we're rollin'

--- roller.js
+++ roller.js
@@ -6,11 +6,39 @@ wod = {
         }
 
         var srList = $('#sr-list');
-        for (var k = 3; k <= 10; k++) {
+        for (var k = 2; k <= 10; k++) {
             srList.append('<option value="' + k + '">SR ' + k + '</option>')
         }
+
+        $('button').click(function (e) {
+            wod.roller();
+            return false;
+        });
     },
     roller: function () {
+        var diceCount = $('#dice-count').val();
+        var sr = $('#sr-list').val();
 
+        var diceResult = $('#dice-result');
+        diceResult.empty();
+        var successCount = 0;
+        for (var k = 0; k < diceCount; k++) {
+            var die = this.roll10();
+            diceResult.append('<div class="pure-u-1-2">' + die + '</div>');
+            // one
+            if (die === 1) {
+                successCount--;
+            } else if (die >= sr) {
+                successCount++;
+                if (die === 10) {
+                    successCount++;
+                }
+            }
+        }
+
+        $('#success-result').html(successCount);
+    },
+    roll10: function () {
+        return 1 + Math.floor(10 * Math.random());
     }
 };
