afa723b06649939f7a95ac2b1dc2a7ebf0986c5c
Trismegiste <flo@trismegiste.net>
2016-03-17 18:55:05 +0100

    view with a table

--- roller.js
+++ roller.js
@@ -23,9 +23,17 @@ wod = {
         });
 
         $('#stat-result button').click(function (e) {
-            var stat = wod.getStat($('#dice-count'), $('#sr-list'));
-            $('#stat-result table').empty();
-            $('#stat-result table').append('<tr><td>wesh</td></tr>');
+            var rolling = 10000;
+            var stat = wod.getStat(rolling, $('#dice-count').val(), $('#sr-list').val());
+            var rowX = $('#stat-result table thead tr');
+            var rowY = $('#stat-result table tbody tr');
+            rowX.empty();
+            rowY.empty();
+            stat.forEach(function (y, x) {
+                rowX.append('<td>' + (x - 1) + '</td>');
+                rowY.append('<td>' + Math.round(y / rolling * 100) + '%</td>');
+            });
+
         });
     },
     updateView: function (rolledDice) {
@@ -89,7 +97,43 @@ wod = {
 
         return res;
     },
-    getStat: function (numDice, targetNumber) {
-        return [];
+    getStat: function (nbRoll, numDice, targetNumber) {
+        var counter = [];
+        for (var n = 0; n < nbRoll; n++) {
+            var res = wod.rollPool(10, numDice, targetNumber);
+            if (res < 0) {
+                res = 0;
+            } else {
+                res++;
+            }
+
+            if (undefined === counter[res]) {
+                counter[res] = 1;
+            } else {
+                counter[res]++;
+            }
+        }
+
+        return counter;
+    },
+    rollDice: function (n) {
+        return 1 + Math.floor(Math.random() * n);
+    },
+    rollPool: function (face, num, target) {
+        var successCount = 0;
+
+        for (var k = 0; k < num; k++) {
+            var die = wod.rollDice(face);
+            if (die === 1) {
+                successCount--;
+            } else if (die >= target) {
+                successCount++;
+                if (die === face) {
+                    successCount++;
+                }
+            }
+        }
+
+        return successCount;
     }
 };
